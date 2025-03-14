252655298d6c6fdaa439cff01f5b2ffcb13838e5
Angelo Auxtero <angeloauxtero17@gmail.com>
2025-03-14 09:54:47 +0800

    update

--- nest-js/src/users/configtable/typeorm.config.ts
+++ nest-js/src/users/configtable/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'myuser',
-  password: 'nget30CJAA',
-  database: 'mydb',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
